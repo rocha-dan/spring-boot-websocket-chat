d0e5f0c65907a765e67c176550025125774691db
Daniel Rocha <daniel_rocha2009@hotmail.com>
2019-08-04 22:17:09 -0300

    Update WebSocketChatConfig.java

--- src/main/java/br/com/rocha/dan/config/WebSocketChatConfig.java
+++ src/main/java/br/com/rocha/dan/config/WebSocketChatConfig.java
@@ -17,7 +17,10 @@ public class WebSocketChatConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("/app");
 		registry.enableStompBrokerRelay("/topic")
-	;
+			.setRelayHost("owl.rmq.cloudamqp.com")
+		.setRelayPort(61613)
+		.setClientLogin("hmsedciv")
+		.setClientPasscode("ThEi8Jme6oQNOU344QovZjVQb830ix-5");
 
 	}
 }
